4914efd6ffbcb8238e0ea08c38b3c8b743ee432e
Cameron Lakenen <lakenen@box.com>
2014-06-14 23:43:44 -0700

    clean up things, use local

--- index.js
+++ index.js
@@ -15,16 +15,21 @@ var levelup = require('levelup'),
     express = require('express'),
     bodyParser = require('body-parser'),
     EventEmitter = require('eemitter'),
-    http = require('http'),
-    https = require('https'),
+    hh = require('http-https'),
     validUrl = require('valid-url'),
     // https = require('https'),
     view = require('box-view').createClient(process.env.BOX_VIEW_API_TOKEN),
     app = express();
 
+var DB = './db/urls-local';
+
 var MAX_REQUEST_DURATION = 100;
 var TEN_MINUTES = 600000;
-http.createServer(app).listen(process.env.PORT);
+
+view.documentsURL = 'http://localhost:8000/1/documents';
+view.sessionsURL = 'http://localhost:8000/1/sessions';
+
+require('http').createServer(app).listen(process.env.PORT);
 console.log('listening on ' + process.env.PORT);
 
 var defaultUploadParams = {
@@ -35,7 +40,7 @@ var defaultUploadParams = {
         duration: 525900000 // 1k years
     };
 
-var db = levelup('./db/urls', {
+var db = levelup(DB, {
     valueEncoding: 'json'
 });
 
@@ -45,6 +50,20 @@ db.on('put', function (k, v) {
     ee.emit(k, v);
 });
 
+function handleError(err, url, res) {
+    console.log(err);
+    var metadata = {
+        error: err
+    };
+    db.put(url, metadata, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    res.json(400, metadata);
+}
+
 function createSession(url, doc, res) {
     console.log('creating session: ' + url, doc);
     view.sessions.create(doc.id, defaultSessionParams, function (err, session) {
@@ -52,17 +71,7 @@ function createSession(url, doc, res) {
         console.log('finished creating session: ' + url);
 
         if (err) {
-            console.log(err);
-            metadata = {
-                error: err
-            };
-            db.put(url, metadata, function (err) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-            });
-            res.json(400, metadata);
+            handleError(err, url, res);
             return;
         }
 
@@ -89,17 +98,51 @@ function createSession(url, doc, res) {
 }
 
 function uploadDoc(url, res) {
+
+    db.put(url, { pending: true, time: Date.now() }, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+
     console.log('uploading: ' + url);
     view.documents.uploadURL(url, defaultUploadParams, function (err, doc) {
         console.log('finished uploading: ' + url);
         if (err) {
-            console.log(err);
-            res.json(400, {
-                error: err
-            });
+            handleError(err, url, res);
             return;
         }
         createSession(url, doc, res);
+    });
+}
+
+function testSession(id, callback) {
+    hh.get(view.sessionsURL + '/' + id + '/view', function (r) {
+        if (r.statusCode === 200) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+}
+
+function handleSession(id, url, res) {
+    console.log('got a session ' +id +', make sure it works...');
+    testSession(id, function (valid) {
+        if (valid) {
+            console.log('session is valid for '+ url + '... go get it!');
+            res.json({
+                session: id
+            });
+        } else {
+            // session is bad... delete it from the db, and tell the client to retry?
+            console.log('bad session for '+ url + '... retry?');
+            db.del(url);
+            res.json({
+                retry: 1
+            });
+        }
     });
 }
 
@@ -109,12 +152,6 @@ function createDBHandler(url, res) {
             // url does not yet exist in the db, so let's create it!
             console.log('could not find '+ url + ' in the db, so trying to convert it');
 
-            db.put(url, { pending: true, time: Date.now() }, function (err) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-            });
             uploadDoc(url, res);
             return;
         }
@@ -122,40 +159,23 @@ function createDBHandler(url, res) {
 
         if (val.session) {
             console.log('already have a session for url: ' + url);
-
-            console.log('let us make sure it works...');
-            https.get('https://view-api.box.com/1/sessions/' + val.session + '/view', function (r) {
-                if (r.statusCode === 200) {
-                    res.json({
-                        session: val.session
-                    });
-                } else {
-                    db.del(url);
-                    res.json({
-                        retry: 1
-                    });
-                }
-            });
-
+            handleSession(val.session, url, res);
         } else if (val.pending) {
             console.log('this url is still converting?: ' + url);
-            console.log(val);
             if (Date.now() - (val.time || 0) > TEN_MINUTES) {
                 console.log('giving up on this one...');
-                db.del(url);
-                res.json(400, {
-                    error: 'failed to convert?'
-                });
+                handleError('the document failed to convert in a reasonable amount of time', url, res);
                 return;
             }
             // we have this url (maybe requested by someone else?), but it's
-            // not done (or errored) yet... tell them to come back in a couple seconds
+            // not done (or errored) yet... wait for a change in the db for this url
             var tmpfn = function (val) {
                 clearTimeout(tid);
                 handler(null, val);
                 ee.off(url, tmpfn);
             };
             var tid = setTimeout(function () {
+                // tell them to retry if it's taking too long...
                 res.json({
                     retry: 1
                 });
@@ -164,9 +184,7 @@ function createDBHandler(url, res) {
             ee.on(url, tmpfn);
         } else {
             console.log('this url had an error: ' + url);
-            res.json(400, {
-                error: val.error || 'unknown error...'
-            });
+            handleError(val.error || 'unknown error...', url, res);
         }
     };
     return handler;
